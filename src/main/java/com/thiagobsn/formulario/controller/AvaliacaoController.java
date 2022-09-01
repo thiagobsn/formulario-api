824064631758ede2f14ba951dc9d12f464fbf668
Thiago Bernardo <thiagobsn@gmail.com>
2022-08-31 22:10:27 -0300

    Popular tabelas de TipoAvaliacao

--- src/main/java/com/thiagobsn/formulario/controller/AvaliacaoController.java
+++ src/main/java/com/thiagobsn/formulario/controller/AvaliacaoController.java
@@ -5,10 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.thiagobsn.formulario.avaliacao.domain.entity.Avaliacao;
+import com.thiagobsn.formulario.avaliacao.dto.CadastroAvaliacaoDTO;
 import com.thiagobsn.formulario.avaliacao.service.AvaliacaoService;
 
 @RestController
@@ -21,6 +24,11 @@ public class AvaliacaoController {
     @GetMapping
     public ResponseEntity<List<Avaliacao>> listarAvaliacoes() {
         return ResponseEntity.ok(avaliacaoService.listarTodos());
+    }
+
+    @PostMapping
+    public ResponseEntity<Avaliacao> cadastrar(@RequestBody CadastroAvaliacaoDTO cadastroAvaliacaoDTO) {
+        return ResponseEntity.ok(avaliacaoService.cadastrar(cadastroAvaliacaoDTO));
     }
     
 }
