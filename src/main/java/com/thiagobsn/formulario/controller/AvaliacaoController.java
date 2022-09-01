cfab7497558c16ce8da7895d403bfcf2ef583c5f
Thiago Bernardo <thiagobsn@gmail.com>
2022-09-01 00:08:03 -0300

    Doc Avaliação

--- src/main/java/com/thiagobsn/formulario/controller/AvaliacaoController.java
+++ src/main/java/com/thiagobsn/formulario/controller/AvaliacaoController.java
@@ -14,6 +14,10 @@ import com.thiagobsn.formulario.avaliacao.domain.entity.Avaliacao;
 import com.thiagobsn.formulario.avaliacao.dto.CadastroAvaliacaoDTO;
 import com.thiagobsn.formulario.avaliacao.service.AvaliacaoService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 @RestController
 @RequestMapping("/api/avaliacoes")
 public class AvaliacaoController {
@@ -21,11 +25,23 @@ public class AvaliacaoController {
     @Autowired
     private AvaliacaoService avaliacaoService;
 
+    @Operation(tags = "Avaliação", summary = "Listar avaliações", description = "Lista todas as avaliações cadastradas.")
+    @ApiResponses(value = { 
+        @ApiResponse(responseCode = "200", description = "Lista de avaliações"),
+        @ApiResponse(responseCode = "400", description = "Dados inválidos"),
+        @ApiResponse(responseCode = "500", description = "Erri interno")
+    })
     @GetMapping
     public ResponseEntity<List<Avaliacao>> listarAvaliacoes() {
         return ResponseEntity.ok(avaliacaoService.listarTodos());
     }
 
+    @Operation(tags = "Avaliação", summary = "Cadastrar nova avaliação", description = "Cadastra uma nova avaliação.")
+    @ApiResponses(value = { 
+        @ApiResponse(responseCode = "200", description = "Cadastro realizado com sucesso"),
+        @ApiResponse(responseCode = "400", description = "Dados inválidos"),
+        @ApiResponse(responseCode = "500", description = "Erri interno")
+    })
     @PostMapping
     public ResponseEntity<Avaliacao> cadastrar(@RequestBody CadastroAvaliacaoDTO cadastroAvaliacaoDTO) {
         return ResponseEntity.ok(avaliacaoService.cadastrar(cadastroAvaliacaoDTO));
